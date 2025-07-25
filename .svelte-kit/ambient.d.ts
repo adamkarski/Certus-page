3adbe8df3c688fc52b01d607aeda24a7008562c1
Adam Karski <zbigniew.karski@gmail.com>
2025-07-25 11:43:59 +0200

    ok

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -70,6 +70,7 @@ declare module '$env/static/private' {
 	export const PATH: string;
 	export const npm_config_argv: string;
 	export const npm_package_dependencies_swiper: string;
+	export const LaunchInstanceID: string;
 	export const _: string;
 	export const __CFBundleIdentifier: string;
 	export const PWD: string;
@@ -117,6 +118,7 @@ declare module '$env/static/private' {
 	export const _P9K_SSH_TTY: string;
 	export const npm_config_init_version: string;
 	export const npm_config_ignore_optional: string;
+	export const SECURITYSESSIONID: string;
 	export const npm_package_dependencies__lottiefiles_svelte_lottie_player: string;
 	export const npm_package_scripts_check: string;
 	export const COLORTERM: string;
@@ -198,6 +200,7 @@ declare module '$env/dynamic/private' {
 		PATH: string;
 		npm_config_argv: string;
 		npm_package_dependencies_swiper: string;
+		LaunchInstanceID: string;
 		_: string;
 		__CFBundleIdentifier: string;
 		PWD: string;
@@ -245,6 +248,7 @@ declare module '$env/dynamic/private' {
 		_P9K_SSH_TTY: string;
 		npm_config_init_version: string;
 		npm_config_ignore_optional: string;
+		SECURITYSESSIONID: string;
 		npm_package_dependencies__lottiefiles_svelte_lottie_player: string;
 		npm_package_scripts_check: string;
 		COLORTERM: string;
