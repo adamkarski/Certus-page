2cdb976de3fb0850729e0d1fcffe6567deec249f
Adam Karski <zbigniew.karski@gmail.com>
2025-07-30 14:38:40 +0200

    responsive start responsive.scss

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -97,6 +97,7 @@ declare module '$env/static/private' {
 	export const SECURITYSESSIONID: string;
 	export const npm_node_execpath: string;
 	export const npm_config_prefix: string;
+	export const NODE_ENV: string;
 }
 
 /**
@@ -199,6 +200,7 @@ declare module '$env/dynamic/private' {
 		SECURITYSESSIONID: string;
 		npm_node_execpath: string;
 		npm_config_prefix: string;
+		NODE_ENV: string;
 		[key: `PUBLIC_${string}`]: undefined;
 		[key: `${string}`]: string | undefined;
 	}
